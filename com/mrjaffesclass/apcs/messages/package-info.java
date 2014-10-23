b13d6a5929061c4d999e0c5980189011cee357a7
Roger Jaffe <rogerjaffe@gmail.com>
2014-10-22 20:20:24 -0700

    Change name to Messenger
    Messages class becomes Messenger
    Messagemailbox becomes MessageHandler

--- com/mrjaffesclass/apcs/messages/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package implements a messaging system used for communication
- * between Model, View, and Controller in a program using an MVC framework.
- * Instantiate an instance of the Messaging class and use the methods listed
- * in the Messaging class to send and subscribe to messages
- */
-package com.mrjaffesclass.apcs.messages;
-// End of file! For package documentation only
